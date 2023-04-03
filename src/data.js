d5bd94cddd52cc00f94076f09db13789e1eca057
Achille MBOUGUENG <achille.mbougueng@externe.maif.fr>
2023-04-03 17:54:03 +0200

    Mis à jour des données

--- src/data.js
+++ src/data.js
@@ -1,6 +1,14 @@
 const DIGIFORMA_API_URL= "https://app.digiforma.com/api/v1/graphiql";
 const DIGIFORMA_API_TOKEN= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjYxMTUsIm1vZGUiOiJhcGkiLCJ0eXBlIjoidXNlciIsImV4cCI6MTk3NDMyNjQwMCwiaXNzIjoiRGlnaWZvcm1hIn0.OnkYrOvYD2r2vwlB6VJBxivs5c-2ZxqjCS61otuiinY";
-const API_URL= "https://admin.esic-online.chillo.fr/items";
+const API_URL= "https://build.esic-online.chillo.fr/items";
+const PROD_API_URL="https://build.esic-online.com/items"
+const PROD_ACCES_TOKEN="7qPinYyvMPKxUOP2GNC-eeraImbWYF4J"
+const delaiAccess = (text) => `
+  <ul>
+  <li>Le délai d'accès moyent pour cette formation est de ${text}</li>
+  <li>Merci d’utiliser le formulaire de contact ou nous joindre par téléphone ou mail pour l’organisation de votre formation.</li>
+  </ul>
+`;
 const DIGIFORMA_HEADERS = {
   'Content-Type': 'application/json',
   'Authorization': `Bearer ${DIGIFORMA_API_TOKEN}`
@@ -8,20 +16,26 @@ const DIGIFORMA_HEADERS = {
 const HEADERS = {
   'Content-Type': 'application/json',
   'Accept': 'application/json',
-  'Authorization': 'Bearer lK7DlJvWeIMK6Yac9C3XsFfYVMcKiLYX'
+  'Authorization': 'Bearer 7qPinYyvMPKxUOP2GNC-eeraImbWYF4J'
+}
+
+const PROD_HEADERS = {
+  'Content-Type': 'application/json',
+  'Accept': 'application/json',
+  'Authorization': 'Bearer 7qPinYyvMPKxUOP2GNC-eeraImbWYF4J'
 }
 const PROGRAM_KEY_MAP = {
   "name":"libelle",
   "subtitle":"souslibelle",
   "description":"contenu",
   "goals":"objectifs",
-  "prerequisites":"etudiants",
+  "prerequisites":"prerequis",
   "enrollingLevel":"niveau",
   "graduationTarget": "niveauDeCertification",
   "cpf":"cpf",
-  "durationInHours":"duree_en_heures",
-  "cpfCode":"cpf_code",
-  "durationInDays":"duree_en_jours",
+  "durationInHours":"heures",
+  "cpfcode":"cpf_code",
+  "durationInDays":"jours",
   "id":"digiforma_id",
   "dpc": "dpc",
   "rncpCode": "rncpCode",
@@ -31,6 +45,320 @@ const CATEGORY_KEY_MAP = {
   "name":"libelle",
   "id":"digiforma_id"
 }
+
+const QUERY_PROGRAMS_BY_ID = (id) => `
+  query {
+    program(id: ${id}) {
+      accessDelay
+      accessDelayUnit
+      accountingAnalytics
+      accountingNumber
+      accountingNumberFundingAgency
+      admissionModality
+      assessments {
+        sourceId
+        text
+      }
+      capacity {
+        active
+        max
+        min
+      }
+      category {
+        color
+        id
+        name
+        order
+        name
+      }
+      certifInfoCode
+      certificateurContratId
+      certificateurId
+      certificationDetails
+      certificationIncludedInAdditionalExpenses
+      certificationModality
+      certifiedData
+      clonedFrom
+      code
+      costs {
+        cost
+        costIndependant
+        costIndividual
+        costMode
+        description
+        id
+        monthly
+        type
+        vat
+      }
+      costsInter{
+        cost
+        costIndependant
+        costIndividual
+        costMode
+        description
+        id
+        monthly
+        type
+        vat
+      }
+      cpf
+      cpfCode
+      createdAt
+      description
+      diploma
+      diplomaTitle
+      documents {
+        downloadable
+        filename
+        id
+        mime
+        size
+        type
+        url
+        wistia {
+          hashedId
+          id
+        }
+      }
+      dpc
+      duration
+      durationDays
+      durationInDays
+      durationInHours
+      economicalModel
+      enrollingLevel
+      enrollingLevelEnforced
+      entryExitModality
+      goals {
+        sourceId
+        text
+      }
+      graduatedLevel
+      graduationModality
+      graduationTarget
+      graduationValidityYears
+      handicappedAccessibility
+      hoursCenter
+      hoursCompany
+      id
+      image {
+        filename
+        id
+        mime
+        size
+        url
+      }
+      language
+      mentoring
+      name
+      onSale
+      overallGoal
+      parentId
+      pedagogicalResources {
+        sourceId
+        text
+      }
+      place
+      placeRegulatoryCompliance
+      prerequisites {
+        sourceId
+        text
+      }
+      publicRegistrationUrl
+      rncpCode
+      rsCode
+      rythm
+      satisfactionDescription
+      showInstructors
+      skillBlock
+      specialty
+      steps {
+        sourceId
+        text
+        substeps {
+          text
+        }
+      }
+      subtitle
+      tags 
+        {
+          description
+          id
+          name
+        }
+      targets {
+        sourceId
+        text
+      }
+      trainingModality
+      trainingPedagogicalModality
+      trainingType
+      transmitterCertificationId
+      version
+      youtubeId
+    }
+  }
+` 
+const QUERY_PROGRAMS_ID = `
+  query {
+    programs {
+      id
+      name
+    }
+  }
+` 
+const QUERY_PROGRAMS_WITH_ALL_PARAMS = `
+  query {
+    programs {
+      accessDelay
+      accessDelayUnit
+      accountingAnalytics
+      accountingNumber
+      accountingNumberFundingAgency
+      admissionModality
+      capacity {
+        active
+        max
+        min
+      }
+      category {
+        color
+        id
+        name
+        order
+        name
+      }
+      certifInfoCode
+      certificateurContratId
+      certificateurId
+      certificationDetails
+      certificationIncludedInAdditionalExpenses
+      certificationModality
+      certifiedData
+      clonedFrom
+      code
+      costs {
+        cost
+        costIndependant
+        costIndividual
+        costMode
+        description
+        id
+        monthly
+        type
+        vat
+      }
+      costsInter{
+        cost
+        costIndependant
+        costIndividual
+        costMode
+        description
+        id
+        monthly
+        type
+        vat
+      }
+      cpf
+      cpfCode
+      createdAt
+      description
+      diploma
+      diplomaTitle
+      documents {
+        downloadable
+        filename
+        id
+        mime
+        size
+        type
+        url
+        wistia {
+          hashedId
+          id
+        }
+      }
+      dpc
+      duration
+      durationDays
+      durationInDays
+      durationInHours
+      economicalModel
+      enrollingLevel
+      enrollingLevelEnforced
+      entryExitModality
+      goals {
+        sourceId
+        text
+      }
+      graduatedLevel
+      graduationModality
+      graduationTarget
+      graduationValidityYears
+      handicappedAccessibility
+      hoursCenter
+      hoursCompany
+      id
+      image {
+        filename
+        id
+        mime
+        size
+        url
+      }
+      language
+      mentoring
+      name
+      onSale
+      overallGoal
+      parentId
+      pedagogicalResources {
+        sourceId
+        text
+      }
+      place
+      placeRegulatoryCompliance
+      prerequisites {
+        sourceId
+        text
+      }
+      publicRegistrationUrl
+      rncpCode
+      rsCode
+      rythm
+      satisfactionDescription
+      showInstructors
+      skillBlock
+      specialty
+      steps {
+        sourceId
+        text
+        substeps {
+          text
+        }
+      }
+      subtitle
+      tags 
+        {
+          description
+          id
+          name
+        }
+      targets {
+        sourceId
+        text
+      }
+      trainingModality
+      trainingPedagogicalModality
+      trainingType
+      transmitterCertificationId
+      version
+      youtubeId
+    }
+  }
+`
+
 const QUERY_PROGRAMS = `
 query {
   programs {
@@ -73,8 +401,15 @@ query {
 
 module.exports = {
   API_URL, 
+  delaiAccess,
+  QUERY_PROGRAMS_BY_ID,
+  QUERY_PROGRAMS_ID,
+  QUERY_PROGRAMS_WITH_ALL_PARAMS,
   QUERY_PROGRAMS, 
   HEADERS, 
+  PROD_API_URL,
+  PROD_ACCES_TOKEN,
+  PROD_HEADERS,
   DIGIFORMA_HEADERS, 
   DIGIFORMA_API_URL, 
   DIGIFORMA_API_TOKEN,
--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ const fetch = require('node-fetch');
 const {
   API_URL, 
   QUERY_PROGRAMS, 
+  QUERY_PROGRAMS_WITH_ALL_PARAMS,
   HEADERS, 
   DIGIFORMA_HEADERS, 
   DIGIFORMA_API_URL, 
   PROGRAM_KEY_MAP,
   CATEGORY_KEY_MAP
 } = require('./data.js');
+const {SOUS_CATEGORIES, CATEGORIES} = require('./souscategories.js');
+
 
 const removeDuplicates = (obj) => {
   obj = obj.filter((value, index, self) =>
@@ -27,24 +30,27 @@ const mapObject = (object, keymap) => {
     const resultKey = keymap[key];
     if(key === "goals" || key === "prerequisites") {
       result[resultKey] = object[key].map(({text}) => `<p>${text}</p>`).join("");
+    } else if(key === "enrollingLevel") {
+      result["niveau"] = "BEGINNER"
     } else {
       result[resultKey] = object[key];
     }
   })
-  return result;
+  return Object.fromEntries(Object.entries(result).filter(([_, v]) => v != null));
 }
 
 const save =  async (object, endpoint, index) => {
   try {
+    delete object["undefined"];
     const response = await fetch(`${API_URL}/${endpoint}`, {
       method: 'post',
-      body: JSON.stringify({...object, status: "published"}),
+      body: JSON.stringify({...object, status: "published", niveau: "BEGINNER"}),
       headers: HEADERS
     });
     const {status} = await response;
     if (status!=200) {
       
-      console.log(status, object.digiforma_id, object.libelle)
+      console.log(object)
     }
   } catch (ex) {
     console.error({ex});
@@ -63,19 +69,23 @@ function paginate (arr, size) {
 
 (async () => {
   try {
-    const response = await axios.post(DIGIFORMA_API_URL, {query: QUERY_PROGRAMS}, { headers: DIGIFORMA_HEADERS });
+    const response = await axios.post(DIGIFORMA_API_URL, {query: QUERY_PROGRAMS_WITH_ALL_PARAMS}, { headers: DIGIFORMA_HEADERS });
     const programs = response.data.data.programs;
     const programsToSave = removeDuplicates(programs);
     const paginatedProgramsToSave = paginate(programsToSave, 200);
     var interval = 5000;
 
+    CATEGORIES.forEach((category, index) => {
+      save(category, "categories",`${index}`)
+    })
+
     // categories
+/*
     const categories = programsToSave.filter(n => n.category != null).map(program => program.category);
     removeDuplicates(categories).forEach((category, index) => {
       const mapped = mapObject(category, CATEGORY_KEY_MAP);
-      save(mapped, "categories",`${index}`)
+      save(mapped, "souscategories",`${index}`)
     })
-
     // Programmes
     paginatedProgramsToSave.forEach((group, index) => {
       setTimeout(function () {
@@ -85,6 +95,7 @@ function paginate (arr, size) {
         })
       }, interval * index);
   })
+  */
   } catch (ex) {
     console.error({ex});
     process.exit(1);
